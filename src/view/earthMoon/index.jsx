4871319e8ab973c9829584369033e72c95d49c24
ddz <dongdezhang01@daojia-inc.com>
2019-01-09 21:14:37 +0800

    update :smeil"

--- src/view/earthMoon/index.jsx
+++ src/view/earthMoon/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Style from './style.scss';
+import Rule from '@/assets/rule.png';
 export default class SVG extends Component {
   render() {
     return (
@@ -7,6 +8,14 @@ export default class SVG extends Component {
         <div className={Style.earth}>
           <div className={Style.moon_wrapper}>
             <div className={Style.moon} />
+          </div>
+        </div>
+        <div className={Style.rule}>
+          <div className={Style.rule_wrapper}>
+            活动规则
+          </div>
+          <div className={Style.jump_wrapper}>
+            <img className={Style.jump_image} src={Rule}/>
           </div>
         </div>
       </div>
